db0e326f3a3b3e685882e697104069ecc072df55
TatianaIlieva <103999490+TatianaIlieva@users.noreply.github.com>
2022-08-11 20:55:00 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route , Outlet } from "react-router-dom";
+import Header from "./Header/Header";
+import Footer from './Footer/Footer';
+import Articles from './Pages/Articles';
+import SingleArticle from './Pages/SingleArticle';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+      return (
+          <>
+          <Routes>
+              <Route path="/" element={<Layout/>}>
+                  <Route index />
+                  <Route path="/articles" element={<Articles />}/>
+                  <Route path="/articles/:slug" element={<SingleArticle/>} />
+              </Route>        
+          </Routes>
+          </>
+      )
+  }
 }
 
-export default App;
+function Layout() {
+  return(
+      <div className="flex flex-wrap min-h-screen">        
+        <header className="w-full">
+            <Header/>
+        </header>
+        <section className="w-full">    
+            <Outlet />
+        </section>
+        <footer className="bg-[#f2f2f2] pb-4 w-full self-end">
+            <Footer/>
+        </footer>
+      </div>
+  )
+}
