9be430132ccb1b4445da840f6aede46ed04974b6
TatianaIlieva <103999490+TatianaIlieva@users.noreply.github.com>
2022-08-11 20:50:21 +0300

    Create Footer.jsx

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+    return (
+        <>
+            <div className="container lg:flex lg:flex-wrap lg:items-center lg:justify-between pt-8 pb-8">
+                <button className="relative block mr-auto ml-auto lg:ml-0 lg:mt-8 mb-2 p-3 pl-16 bg-[#fd6d61] text-white font-medium text-base uppercase text-center rounded-[8px] transition-all hover:opacity-70 shadow before:absolute before:w-16 before:top-0 before:bottom-0 before:left-0 before:bg-[url(data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAABYAAAAYCAMAAADJYP15AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAt1BMVEX9bWH9bWH+2tf+trD9bWH+pJz9kYj+0c3+yMP9f3T9bWH+yMT9bWH+trD9dmv9f3T9mpL9bWH9iH79bWH+pJz+rab+2tf9f3T+trD9bWH+rab9o5z9bWH/2tf9kYj9bWH+trD9o5z+v7r+yMT9dmv9bWH+yMP9bWH9iH79kYj9bWH9bWH+trD9dmv9bWH9mpL9f3T9f3X9kYj/7ev9dmv+4+H/////9vX+0c3/2tf/5OH9o5z+2tfuOw01AAAAL3RSTlMA6Pn5TqCE9Nho9diVvFx8klmIjKaz9HHAqefcovSLpfGm+P2H9/1wrb5Lu/vXnw5F0lcAAAABYktHRDZHv4jRAAAAB3RJTUUH5ggLAzUFO6UWnwAAAMpJREFUKM+F0skSgjAMANCq4I5adwXFHXcLwSDo/3+XjR5sqzPm0szLTJekjLGcDGaGtIv4waEI8wXLMjkCEV/BNgq5CMkRipbB5CXEcsXSWXq1hlrhxdLr5Og0NEZo1h1aW5xzhRHa5HanC12uMCbS7U5PZv0B/7B0xobvdPCXk/Zo7HrkkylXjvRniK43hylXL+gvaHW1C4Lwl7TRbKU/fk26WQVGq26I6TYwG5thugv25hgyuB9UlByL8HE8Hc5fI47i7xH//hBP+QkxDko9W3AAAAAASUVORK5CYII=)] before:bg-no-repeat before:bg-center before:content-['']">Download App Now</button>
+                <div className="mt-8 lg:mt-0 text-center lg:text-right">
+                    <ul>
+                        <li className="block md:inline-block md:mr-6 md:ml-6">Trending</li>
+                        <li className="block md:inline-block md:mr-6 md:ml-6">Resources</li>
+                        <li className="block md:inline-block md:mr-6 md:ml-6">Nearby</li>
+                        <li className="block md:inline-block md:mr-6 md:ml-6">Hackathons</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="text-center">
+                Made with &hearts; by VinnovatelT
+            </div>
+        </>
+    )
+}
