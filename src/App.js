f162511ac210b0f0bb3b619bde59eaa858d50d23
TatianaIlieva <103999490+TatianaIlieva@users.noreply.github.com>
2022-08-15 16:23:31 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Routes, Route , Outlet } from "react-router-dom";
-import Header from "./Header/Header";
-import Footer from './Footer/Footer';
 import Articles from './Pages/Articles';
 import SingleArticle from './Pages/SingleArticle';
 
@@ -23,16 +21,10 @@ export default class App extends React.Component {
 
 function Layout() {
   return(
-      <div className="flex flex-wrap min-h-screen">        
-        <header className="w-full">
-            <Header/>
-        </header>
+      <div className="flex flex-wrap min-h-screen">       
         <section className="w-full">    
             <Outlet />
         </section>
-        <footer className="bg-[#f2f2f2] pb-4 w-full self-end">
-            <Footer/>
-        </footer>
       </div>
   )
 }
