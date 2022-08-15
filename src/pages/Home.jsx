54e75d94ae9285ce845558c981a25064739ea079
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-08-15 15:57:23 +0300

    gym section half done - responsive issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
 import TrendingSection from "../components/TrendingSection";
 import NearbySection from "../components/NearbySection";
+import GymnasiumSection from "../components/GymnasiumSection";
 import Footer from "../components/Footer";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <HeroSection />
       <TrendingSection/>
       <NearbySection/>
+      <GymnasiumSection/>
       <Footer/>
     </div>
   );
