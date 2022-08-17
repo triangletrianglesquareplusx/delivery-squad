28b2d188d8968112c9328cf498233a07c95507b3
TatianaIlieva <103999490+TatianaIlieva@users.noreply.github.com>
2022-08-17 18:51:31 +0300

    Update SingleArticle.jsx

--- src/Pages/SingleArticle.jsx
+++ src/Pages/SingleArticle.jsx
@@ -4,19 +4,19 @@ import { articles } from "../articles"
 import { Link } from "react-router-dom"
 
 export default function SingleArticle() {
-    const {slack} = useParams();
-    const SingleArticle = articles.find(el => el.slack === slack);
+    const {slug} = useParams();
+    const SingleArticle = articles.find(el => el.slug === slug);
     
     return (
         <div className='container mt-8 mb-8 text-center'>
             <div className='details-img'>
-                <img className="block mr-auto mb-8 ml-auto max-w-full" src={SingleArticle.Image} alt={SingleArticle.Title} />
+                <img className="block mx-auto mb-8 max-w-full" src={SingleArticle.Image} alt={SingleArticle.Title} />
             </div>
             <div className='details-info'>
-                <h1 className="relative block mb-6 font-bold text-2xl md:text-4xl text-center after:block after:bg-[#fd6d61] after:mt-3 after:ml-auto after:mr-auto after:mb-0 after:w-20 md:after:w-28 after:h-1 md:after:h-1.5 after:rounded-md after:content-['']">{SingleArticle.Title}</h1>
+                <h1 className="relative block mb-6 font-bold text-2xl md:text-4xl text-center after:block after:bg-coral after:mt-3 after:ml-auto after:mr-auto after:mb-0 after:w-20 md:after:w-28 after:h-1 md:after:h-1.5 after:rounded-md after:content-['']">{SingleArticle.Title}</h1>
                 <p className="text-sm md:text-base">{SingleArticle.Description}</p>
             </div>
-            <Link className="block mt-8 mr-auto ml-auto mb-8 p-3 w-56 bg-[#fd6d61] text-white font-medium text-base uppercase text-center rounded-[40px] transition-all hover:bg-black" to={`/articles`}>Back</Link>
+            <Link className="block mt-8 mx-auto mb-8 p-3 w-56 bg-coral text-white font-medium text-base uppercase text-center rounded-some transition-all hover:bg-black" to={`/articles`}>Back</Link>
         </div>
     )
 }
