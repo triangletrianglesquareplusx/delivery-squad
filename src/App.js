996f138a2268ad90a1b918ae6ff9314c07cab1c4
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-08-18 21:35:15 +0300

    testing functionality

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import FilterSortBand from './Components/FilterSortBand/FilterSortBand';
 import MiddleSection from './Components/MiddleSection/MiddleSection';
 import Restaurants from './Pages/Restaurants/Restaurants';
 import Searchbar from './Components/Searchbar/Searchbar';
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 function App() {
   return (
     <>
-      <MiddleSection />
-      <Searchbar />
-      <FilterSortBand />
-      <Restaurants />
+    <Header/>
+    <Footer/>
     </>
   );
 }
