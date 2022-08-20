4e1d04a6bc434039964ce63c8ea0dfabb181eb04
kalinavel <kalinavelichkova@gmail.com>
2022-08-20 19:09:18 +0300

    complete tailwind integration

--- src/Components/Searchbar/Searchbar.jsx
+++ src/Components/Searchbar/Searchbar.jsx
@@ -4,11 +4,11 @@ export default function Searchbar() {
   return (
     <div className="flex items-center justify-center mt-6 search-container">
       <div className="flex items-center p-3 bg-gray-200 rounded-lg h-9 w-96 searchbar">
-        <img src={require("../../Assets/search-resto.png")} alt="search icon" className="search-icon" />
+        <img src={require("../../Assets/search-resto.png")} alt="search icon" className="w-4 h-4 search-icon" />
         <input 
           type="text" 
           placeholder="Search Restaurants"
-          className="flex-none flex-grow-0 order-1 text-sm not-italic font-medium text-center text-gray-500 bg-gray-200 cursor-pointer"
+          className="flex-none flex-grow-0 order-1 text-xs not-italic font-medium text-center text-gray-500 bg-gray-200 cursor-pointer"
           // value={search} 
           // onChange={handleSearchChange}
           />
