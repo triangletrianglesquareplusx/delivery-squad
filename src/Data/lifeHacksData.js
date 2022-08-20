a5e2baa6abc0df04b354b77ea1850bcb5f7b855a
Beatris <beti16b@gmail.com>
2022-08-20 18:05:31 +0300

    LifeHacks page

---
+++ src/Data/lifeHacksData.js
@@ -0,0 +1,38 @@
+export const lifeHacks = [
+    {
+        "id": "1",
+        "number": "1",
+        "title": "Wifi after closing time",
+        "description": "Turn on proxy before 12:30 to get wifi even after it goes offline.",
+    },
+    {
+        "id": "2",
+        "number": "2",
+        "title": "Wifi after closing time",
+        "description": "Turn on proxy before 12:30 to get wifi even after it goes offline.",
+    },
+    {
+        "id": "3",
+        "number": "3",
+        "title": "Wifi after closing time",
+        "description": "Turn on proxy before 12:30 to get wifi even after it goes offline.",
+    },
+    {
+        "id": "4",
+        "number": "4",
+        "title": "Wifi after closing time",
+        "description": "Turn on proxy before 12:30 to get wifi even after it goes offline.",
+    },
+    {
+        "id": "5",
+        "number": "5",
+        "title": "Wifi after closing time",
+        "description": "Turn on proxy before 12:30 to get wifi even after it goes offline.",
+    },
+    {
+        "id": "6",
+        "number": "6",
+        "title": "Wifi after closing time",
+        "description": "Turn on proxy before 12:30 to get wifi even after it goes offline.",
+    },
+]
