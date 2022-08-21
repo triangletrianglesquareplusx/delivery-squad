fc15be360f7acb24ad9dffb4261a8abe97c2d687
kalinavel <kalinavelichkova@gmail.com>
2022-08-21 19:08:00 +0300

    first commit for FAQ page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './FAQ.css'
+import TitleFAQ from './Components/TitleFAQ/TitleFAQ';
+import SearchbarFAQ from './Utilities/SearchbarFAQ/SearchbarFAQ';
+import DropdownFAQ from './Components/DropdownFAQ/DropdownFAQ';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TitleFAQ />
+      <SearchbarFAQ />
+      <DropdownFAQ />
     </div>
   );
 }
---
+++ src/Components/DropdownFAQ/DropdownFAQ.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+
+function DropdownFAQ() {
+  return (
+    <div>
+        <section class="text-gray-700">
+        <div class="container px-5 py-24 mx-auto">  
+          <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+            <div class="w-full lg:w-1/2 px-4 py-2">
+              <details class="mb-4">
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                  How Long is this site live?
+                </summary>
+
+                <span>
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+              <details class="mb-4">
+                <summary class="font-semibold bg-gray-200 rounded-md py-2 px-4">
+                  Can I install/upload anything I want on there?
+                </summary>
+
+                <span>
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+              <details class="mb-4">
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                  How can I migrate to another site?
+                </summary>
+
+                <span>
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+            </div>
+            <div class="w-full lg:w-1/2 px-4 py-2">
+              <details class="mb-4">
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                  Can I change the domain you give me?
+                </summary>
+
+                <span class="px-4 py-2">
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+              <details class="mb-4">
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                  How many sites I can create at once?
+                </summary>
+
+                <span class="px-4 py-2">
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+              <details class="mb-4">
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                  How can I communicate with you?
+                </summary>
+
+                <span class="px-4 py-2">
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default DropdownFAQ
