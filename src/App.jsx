7e059271404949a26fe948aace05f3d395fc75cb
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-08-23 16:40:49 +0300

    faq page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import './Restaurants.css';
-import { Routes, Route , Outlet } from "react-router-dom";
-import Articles from './Pages/Articles';
-import SingleArticle from './Pages/SingleArticle';
-import RestaurantsFullPage from './Pages/RestaurantsFullPage';
-import Header from './Components/Header'
-import Footer from './Components/Footer'
-import Home from './Pages/Home'
-import Shopping from './Components/Shopping/Shopping'
+import React from "react";
+import "./Restaurants.css";
+import { Routes, Route, Outlet } from "react-router-dom";
+import Articles from "./Pages/Articles";
+import SingleArticle from "./Pages/SingleArticle";
+import RestaurantsFullPage from "./Pages/RestaurantsFullPage";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Pages/Home";
+import Shopping from "./Components/Shopping/Shopping";
+import FAQFullPage from "./Pages/FAQFullPage";
 
 export default function App() {
   return (
     <>
-          <Header/>
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/articles" element={<Articles/>}/>
-            <Route path="/articles/:slug" element={<SingleArticle/>} />
-            <Route path="/restaurants" element={<RestaurantsFullPage/>}/>
-            <Route path="/shopping" element={<Shopping/>}/>
-          </Routes>
-          <Footer/>
-     </>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/articles" element={<Articles />} />
+        <Route path="/articles/:slug" element={<SingleArticle />} />
+        <Route path="/restaurants" element={<RestaurantsFullPage />} />
+        <Route path="/shopping" element={<Shopping />} />
+        <Route path="/faq" element={<FAQFullPage />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
-
-
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 import ControlButton from "../Utilities/ControlButton";
 import React from "react";
 import { MdLocationPin } from "react-icons/md";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div className={`flex justify-between items center p-3 w-10/12 mx-auto`}>
@@ -24,20 +24,22 @@ export default function Header() {
             className={`hidden md:inline md:px-6 md:py-3 md:font-medium hover:text-regalBlue`}
           />
         </Link>
-        
-          <ControlButton
-            name={"Nearby"}
-            className={`hidden md:inline md:px-6 md:py-3 md:font-medium hover:text-regalBlue`}
-          />
-        
+
+        <ControlButton
+          name={"Nearby"}
+          className={`hidden md:inline md:px-6 md:py-3 md:font-medium hover:text-regalBlue`}
+        />
+
         <ControlButton
           name={"Hackatons"}
           className={`hidden md:inline md:px-6 md:py-3 md:font-medium hover:text-regalBlue`}
         />
-        <ControlButton
-          name={"FAQ"}
-          className={`hidden md:inline md:px-6 md:py-3 md:font-medium hover:text-regalBlue`}
-        />
+        <Link to="/faq">
+          <ControlButton
+            name={"FAQ"}
+            className={`hidden md:inline md:px-6 md:py-3 md:font-medium hover:text-regalBlue`}
+          />
+        </Link>
         <ControlButton
           name={"Download App"}
           className={`px-6 py-2 whitespace-no-wrap bg-primaryRed rounded-lg shadow-md text-white text-sm font-medium font-inter`}
