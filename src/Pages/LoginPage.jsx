ee6e06de9ca24bc93320514257ad1953c130c6c4
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-08-29 00:17:02 +0300

    basic react-hook-form added

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -2,10 +2,22 @@ import React from "react";
 import ControlButton from "../Utilities/ControlButton";
 import { HiLightBulb } from "react-icons/hi";
 import { useForm } from "react-hook-form";
+
 function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className="flex items-start justify-center h-screen m-10">
-      <div className="container flex flex-col w-4/12 gap-2 p-10 text-sm rounded-lg shadow-lg bg-scaledDownWhite">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="container flex flex-col items-center w-4/12 gap-2 p-10 text-sm rounded-lg shadow-lg bg-scaledDownWhite"
+      >
         <div className="flex justify-center gap-3 text-regalBlue">
           <HiLightBulb />
           <p className="text-sm text-black">
@@ -14,14 +26,28 @@ function LoginPage() {
           </p>
         </div>
         <p className="text-sm text-center">Email address</p>
-        <div className="flex justify-center">
-          <input className="p-2 bg-blue-200 rounded-lg outline-none" />
+        <div className="flex flex-col w-9/12">
+          <input
+            {...register("email", { required: true })}
+            className="p-2 bg-blue-200 rounded-lg outline-none"
+          />
         </div>
         <p className="text-sm text-center">Password</p>
-        <div className="flex justify-center">
-          <input className="p-2 bg-blue-200 rounded-lg outline-none" />
+        <div className="flex flex-col w-9/12 gap-2">
+          <input
+            {...register("passwordRequired", {
+              required: true,
+              minLength: {
+                value: 10,
+                message: "You need a 10 digit pass minimum!",
+              },
+            })}
+            className="p-2 bg-blue-200 rounded-lg outline-none"
+          />
+          <p className="text-red-600 ">{errors.passwordRequired?.message}</p>
         </div>
-      </div>
+        <input type="submit" />
+      </form>
     </div>
   );
 }
