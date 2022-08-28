98fb1e432d9822386d64c7010f97d0e2164340e2
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-08-28 23:28:19 +0300

    styled form done

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -4,21 +4,22 @@ import { HiLightBulb } from "react-icons/hi";
 import { useForm } from "react-hook-form";
 function LoginPage() {
   return (
-    <div className="flex items-start justify-center h-screen p-10 m-10">
-      <div className="w-3/12 rounded-lg shadow-lg bg-mutedGray">
-        <div className="p-10 text-sm">
-          <div className="flex">
-            <div className="flex items-center gap-2">
-              <HiLightBulb className="text-3xl" />
-              <p className="text-regalBlue">Speak friend and enter.</p>
-            </div>
-            <div className="text-xs font-light">
-              <p>Don't have an account?</p>
-              <p>Sign up</p>
-            </div>
-          </div>
-
-          <p>Hey this will be a login page</p>
+    <div className="flex items-start justify-center h-screen m-10">
+      <div className="container flex flex-col w-4/12 gap-2 p-10 text-sm rounded-lg shadow-lg bg-scaledDownWhite">
+        <div className="flex justify-center gap-3 text-regalBlue">
+          <HiLightBulb />
+          <p className="text-sm text-black">
+            Speak <span className="text-sm text-regalBlue">'friend'</span> and
+            enter.
+          </p>
+        </div>
+        <p className="text-sm text-center">Email address</p>
+        <div className="flex justify-center">
+          <input className="p-2 bg-blue-200 rounded-lg outline-none" />
+        </div>
+        <p className="text-sm text-center">Password</p>
+        <div className="flex justify-center">
+          <input className="p-2 bg-blue-200 rounded-lg outline-none" />
         </div>
       </div>
     </div>
