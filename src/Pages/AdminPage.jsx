c35ce85e352f91a8faea0066d320c26a165bdd6d
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-09-01 10:32:30 +0300

    adding useDispatch

--- src/Pages/AdminPage.jsx
+++ src/Pages/AdminPage.jsx
@@ -1,7 +1,8 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 function AdminPage() {
   const email = useSelector((state) => state.user.email);
+  const dispatch = useDispatch();
   return (
     <div className="h-screen m-10">
       <p>You have authenticated! Welcome to the admin page. {email}</p>
