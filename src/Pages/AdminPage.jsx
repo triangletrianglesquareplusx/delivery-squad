7868765a95689584d3a536d19f7a3f523b1585a5
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-09-13 16:22:53 +0300

    testing new branch from head detached

--- src/Pages/AdminPage.jsx
+++ src/Pages/AdminPage.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function AdminPage() {
   return (
     <div className="h-screen m-10">
-      <p>You have authenticated! Welcome to the admin page.</p>
+      <p>
+        You have authenticated! Welcome to the admin page. This is a new branch
+      </p>
     </div>
   );
 }
